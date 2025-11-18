d9bed117464ca26f3118293ca12f9fad0c945a18
Abhijeet Chauhan <abhijeetchauhan025@gmail.com>
2025-11-18 17:01:19 +0530

    commit

--- app/(root)/index.jsx
+++ app/(root)/index.jsx
@@ -1,13 +1,27 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Link, useRouter } from "expo-router";
+import { Alert, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from '../../components/SignOutButton'
 import { useTransactions } from '../../hooks/useTransactions'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Ionicons } from "@expo/vector-icons";
+import PageLoader from '../../components/PageLoader'
+import { BalanceCard } from '../../components/BalanceCard'
+import { styles } from '../../assets/styles/home.styles'
+import { TransactionItem } from "../../components/TransactionItem";
+import NoTransactionsFound from "../../components/NoTransactionsFound";
 
 export default function Page() {
   const { user } = useUser()
+  const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
   const {transactions, summary, isLoading, loadData, deleteTransaction} = useTransactions(user.id)
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadData();
+    setRefreshing(false);
+  };
 
   useEffect(() => {
     loadData()
@@ -17,23 +31,53 @@ export default function Page() {
   console.log('Transactions:', transactions)
   console.log('Summary:', summary)
 
+  const handleDelete = (id) => {
+    Alert.alert("Delete Transaction", "Are you sure you want to delete this transaction?", [
+      { text: "Cancel", style: "cancel" },
+      { text: "Delete", style: "destructive", onPress: () => deleteTransaction(id) },
+    ]);
+  };
+
+  if (isLoading && !refreshing) return <PageLoader />;
+
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>{summary.income}</Text>
-        <Text>{summary.balance}</Text>
-        <Text>{summary.expenses}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Image source={require('../../assets/images/logo.png')} style={styles.headerLogo} resizeMode="contain" />
+          </View>
+          <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+            <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+
+        </View>
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+
+      </View>
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        renderItem={({ item }) => <TransactionItem item={item} onDelete={handleDelete} />}
+        ListEmptyComponent={<NoTransactionsFound />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
     </View>
   )
 }
